f090dfb4ba0a2469bd2c8fadc6ee8a2750671271
Alba Raquel Velasco González <albaraquelvelascogonzalez@MacBook-Air-de-Alba.local>
2023-06-20 17:26:47 +0200

    test component remove from app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import CardComponentContainer from '../src/components/CardComponentContainer/CardComponentContainer'
+
 
 function App() {
   return (
     <div className="App">
-      <CardComponentContainer />
+     
     </div>
   );
 }
