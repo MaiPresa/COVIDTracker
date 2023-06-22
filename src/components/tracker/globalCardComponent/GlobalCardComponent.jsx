20f7ff959e4514573a114226bb5063411f39f125
Alba Velasco <albavelascoglez@gmail.com>
2023-06-22 20:19:27 +0200

    globalCardComponent Done

--- src/components/tracker/globalCardComponent/GlobalCardComponent.jsx
+++ src/components/tracker/globalCardComponent/GlobalCardComponent.jsx
@@ -1,9 +1,19 @@
-export function GlobalCardComponent(props) {
-    return(
-        <>
-            <img src={props.covidIcon} />
-            <h3>{props.globalTitle}</h3>
-            <p id={props.globalId}></p>
-        </>
-    )
+import './globalCardComponent.scss'
+
+function GlobalCardComponent(props) {
+  return (
+    <>
+      <div className='globalCard'>
+        <div className='globalIcon'>
+        <img src={props.covidIcon} className='covidIcon'/>
+        </div>
+      <div className="globalData">
+        <p className='globalDataTitle'>{props.globalTitle}</p>
+        <p id={props.globalId} className='globalIdData'>435435345</p>
+      </div>
+      </div>
+    </>
+  );
 }
+
+export default GlobalCardComponent;
