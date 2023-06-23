d7a396c3f180143b1356ae30f63de587f28849b0
Rocio <rocioalonso.dev@gmail.com>
2023-06-23 10:58:37 +0200

    cleaned App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import { NavbarComponent } from './components/tracker/navbarComponent/NavbarComponent';
-
-
 
 function App() {
   return (
     <div className="App">
-      <NavbarComponent></NavbarComponent>
+      
     </div>
   );
 }
