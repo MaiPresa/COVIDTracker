109d2ab20ca676d2300b9b86ee19c478a9625d08
MaiPresa <maite_presa@hotmail.com>
2023-06-25 20:34:57 +0200

    landingPageMVP

---
+++ src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,54 @@
+import "./LandingPage.scss";
+
+import {NavBar} from "../../components/landingPage/navBar/NavBar";
+import { Btn } from "../../components/button/Btn";
+import { Banner } from "../../components/landingPage/banner/Banner";
+import LogosBannerComponent from "../../components/landingPage/logosBannerComponent/LogosBannerComponent";
+import CardComponentContainer from "../../components/landingPage/CardComponentContainer/CardComponentContainer";
+
+import bannerImage from "../../assets/images/phones.png";
+import secondBannerImage from "../../assets/images/mobiles.png";
+
+
+export const LandingPage = () => {
+    return(
+        <>
+            <NavBar/>
+            <header className="landingHeader">
+                <Banner 
+                    title={["Download the Covid", <br/>, "App for get latest", <br/>, "update"]}
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula 
+                    est id purus facilisis, eu vestibulum quam tempus. Proin tristique odio a velit 
+                    congue porta. In maximus ante sit amet diam viverra commodo. Donec sagittis placerat 
+                    dui et sollicitudin." 
+                    image={bannerImage}>
+                    <Btn btnType="btnSecondary">HOW TO PROTECT</Btn>
+                </Banner>
+            </header>
+
+            <section className="logosSection">
+                <LogosBannerComponent/>
+            </section>
+
+            <section className="landingFirstSection">
+                <p>App feature</p>
+                <h2>Basic Feature You Will Get<br/>When You Use</h2>
+                <CardComponentContainer/>
+            </section>
+
+            <section className="landingSecondSection">
+                <Banner 
+                    title={["Stay Home, And", <br/>, "Prayer For Victim"]}
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula 
+                    est id purus facilisis, eu vestibulum quam tempus. Proin tristique odio a velit 
+                    congue porta. In maximus ante sit amet diam viverra commodo. Donec sagittis placerat 
+                    dui et sollicitudin."
+                    image={secondBannerImage}
+                    order = "landingBannerOrder">
+                    <Btn btnType="btnSquarePrimary">DOWNLOAD NOW</Btn>
+                </Banner>
+            </section>
+        </>
+
+    )
+}
