3b323ee606374f408d3534b20e226b48b3cc6cfb
Rocio <rocioalonso.dev@gmail.com>
2023-06-26 14:02:16 +0200

    desktop display setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {TrackerGlobalPage} from './pages/tracker/TrackerGlobalPage'
 
 function App() {
   return (
     <div className="App">
-      
+      <TrackerGlobalPage />
     </div>
   );
 }
