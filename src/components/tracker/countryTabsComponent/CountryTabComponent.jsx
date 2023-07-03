888479e544ed6163d94ddbf33d35fe3e3fa88711
Rocio <rocioalonso.dev@gmail.com>
2023-07-03 20:06:15 +0200

    styles and functionalities for tracker Tabs

--- src/components/tracker/countryTabsComponent/CountryTabComponent.jsx
+++ src/components/tracker/countryTabsComponent/CountryTabComponent.jsx
@@ -3,7 +3,7 @@ import "./countryTabComponent.scss";
 export const CountryTabComponent = (props) => {
 
     return(
-        <div className="tab" value={props.value} key={props.key}>
+        <div className="tab" value={props.value} onClick={props.onClick}>
           <img src={props.countryFlag} className="flagCircle" alt="flag"></img>
           <p className="topTenCountry">{props.country}</p>
         </div>
--- src/pages/tracker/fourthViewOutlet/FourthViewOutlet.jsx
+++ src/pages/tracker/fourthViewOutlet/FourthViewOutlet.jsx
@@ -1,12 +1,14 @@
 import "./fourthViewOutlet.scss";
 import { CountryTabsContainer } from "../../../components/tracker/countryTabsContainer/CountryTabsContainer";
 import  CountryCardsComponent  from "../../../components/tracker/countryCardsComponent/CountryCardsComponent";
+
 export const FourthViewOutlet = () => {
 
+    
 
     return(
         <div id="tabs">
-            <h1>Covid-19 Country Wise - Tabs</h1>
+            <h1 id='tabsTitle'>Covid-19 Country Wise - Tabs</h1>
             <hr />
             <CountryTabsContainer />
             <CountryCardsComponent />
